2aecaac64788a265fba2fe1fbbf8087c83905cf9
Jose Ricaurte <jricaurte1985@gmail.com>
2016-07-01 23:15:44 -0400

    deleted pivotal server folder and deleted import statement

--- src/main/java/com/ims/util/IMSCtrl.java
+++ src/main/java/com/ims/util/IMSCtrl.java
@@ -2,7 +2,7 @@ package com.ims.util;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
 @RequestMapping("/home")
